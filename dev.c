84a0d5aed5251a4551e85a0d126b0bd727100469
guray00 <lampis.marco2000@gmail.com>
2022-10-15 21:53:19 +0200

    caricato file dev

---
+++ dev.c
@@ -0,0 +1,89 @@
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <signal.h>
+#include <termios.h>
+#include <ctype.h>
+
+#include "./device/dev_gui.h"
+#include "./device/dev_net.h"
+#include "./API/logger.h"
+#include "./utils/costanti.h"
+#include "./utils/connection.h"
+
+#define SIZE 15
+
+int to_child_fd[2];
+int to_parent_fd[2];
+int pid;
+int DEVICE_PORT;
+struct connection* con;
+
+
+// recupera la porta inserita in fase di avvio
+int portCheck(int argc, char* argv[]){
+    if(argc < 2){
+        printf("Errore, porta non specificata\n");
+        exit(-1);
+    }
+
+    return atoi(argv[1]);
+}
+
+int main(int argc, char* argv[])
+{   
+    // recupera la porta sulla quale si comunica
+    DEVICE_PORT = portCheck(argc, argv);
+     
+    // inizializzo la testa di con
+    con = (void*) malloc(sizeof(struct connection));
+    con->port = DEVICE_PORT;
+    con->prev = NULL;
+    con->next = NULL;
+
+    // inizializzo il logger
+    init_logger("./.log");
+
+
+    // realizzo la pipe in modo
+    // che i processi possano
+    // comunicare tra di loro
+    pipe(to_child_fd);
+    pipe(to_parent_fd);
+
+    // divido il processo in 
+    // frontend e backend
+    pid = fork();
+
+    // frontend (child)
+    if (pid == 0) {
+        // chiusura fd inutilizzati
+        close(to_child_fd[1]); 
+        close(to_parent_fd[0]);  
+        
+        // avvio il frontend del 
+        // device per la gestione
+        // dell'interfaccia
+        startGUI();
+    } 
+    
+    // backend (father)
+    else {
+        // chiusura fd inutilizzati
+        close(to_parent_fd[1]); 
+        close(to_child_fd[0]);
+
+        // avvio il backend del device
+        // per la gestione delle richieste
+        // di rete
+        startNET();
+    }
+
+    return 0;
+}
